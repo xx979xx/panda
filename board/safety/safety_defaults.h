9c6048e0764f7a746ce26b2afea77c4c0fccb28b
xx979xx <xx979xx@github.com>
2020-11-09 12:49:44 +0300

    Smart MDPS init

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -1,3 +1,15 @@
+bool speed_spoof_active = false;
+
+void spoof_speed(CAN_FIFOMailBox_TypeDef *to_fwd){
+  bool mph_speed = GET_BYTE(to_fwd, 2) & 0x2;
+  int enable_speed = mph_speed ? 38 : 60;
+  int speed = GET_BYTE(to_fwd, 1) | (GET_BYTE(to_fwd, 2) & 0x1) << 8;
+  if (speed < enable_speed) {
+    to_fwd->RDLR &= 0xFFFE00FF;
+    to_fwd->RDLR |= enable_speed << 8;
+  }
+};
+
 int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   UNUSED(to_push);
   return true;
@@ -24,9 +36,19 @@ static int nooutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
 }
 
 static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
-  UNUSED(bus_num);
-  UNUSED(to_fwd);
-  return -1;
+  int addr = GET_ADDR(to_fwd);
+  int bus_fwd = -1;
+
+  if (bus_num == 0) {
+    bus_fwd = 2;
+    if (addr == 1265 && speed_spoof_active) {
+      spoof_speed(to_fwd);
+    }
+  }
+  if (bus_num == 2) {
+    bus_fwd = 0;
+  }
+  return bus_fwd;
 }
 
 const safety_hooks nooutput_hooks = {
